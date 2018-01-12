dc548691df3736680da6c557e03a2b27e207cf72
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-12 20:39:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,5 +19,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/^[1-9]{10}$|^.[1-9]{3}.[1-9]{3}.[1-9]{4}$|^[1-9]{3}.[1-9]{3}.[1-9]{4}$/) ? true : false
+  phone.match(/^[0-9]{10}$|^.[0-9]{3}.[0-9]{3}.[0-9]{4}$|^[0-9]{3}.[0-9]{3}.[0-9]{4}$|^.[0-9]{3}.[0-9]{7}$/) ? true : false
 end
